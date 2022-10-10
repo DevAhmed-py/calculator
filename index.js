ab4bcb62c3130eb67b0b06d5670c34d8035ee994
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-10 20:09:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let num1 = 10
+let num2 = 2
+
+document.getElementById("num1").textContent = num1
+document.getElementById("num2").textContent = num2
+let sum = document.getElementById("sum")
+
+function add() {
+    let result = num1 + num2
+    sum.textContent = "Result: " + result
+}    
+
+function subtract() {
+    let result = num1 - num2
+    sum.textContent = "Result: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sum.textContent = "Result: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sum.textContent = "Result: " + result
+}
